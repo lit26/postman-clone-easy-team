5b54ecd17e147a870bf691d9e944915bb016b7aa
Tianning Li <ltianningli@gmail.com>
2022-03-30 21:32:52 -0700

    fix wording

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -13,7 +13,7 @@ import SidebarNav from "./SidebarNav";
 const Sidebar: React.FC = () => {
   useEffect(() => {
     alert(
-      "This is a demo app will behave a little bit different than the actual app. All the changes in this app will not update the database. "
+      "This demo app will behave a little bit different than the actual app. All the changes in this app will not update the database. "
     );
   }, []);
   const {
